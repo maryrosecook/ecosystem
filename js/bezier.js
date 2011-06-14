91e9abd927474f2be4b4f7bae361e320d8ae0d0b
Mary Rose Cook <maryrosecook@maryrosecook.com>
2011-06-14 19:01:03 +0100

    ants can use astar to find their way around

--- js/bezier.js
+++ js/bezier.js
@@ -5,8 +5,33 @@ Coordinate.create = function(x, y) {
     var coordinate = new Coordinate();
     coordinate.x = x;
     coordinate.y = y;
+    coordinate.id = x + "," + y;
 
     return coordinate;
+}
+Coordinate.prototype = {
+    neighbours: function(maxCoordinates) {
+        var neighbours = [];
+
+        if(this.x > 0)
+            neighbours.push(Coordinate.create(this.x - 1, this.y));
+        if(this.y > 0)
+            neighbours.push(Coordinate.create(this.x, this.y - 1));
+        if(this.x < maxCoordinates.x)
+            neighbours.push(Coordinate.create(this.x + 1, this.y));
+        if(this.y < maxCoordinates.y)
+            neighbours.push(Coordinate.create(this.x, this.y + 1));
+        if(this.x > 0 && this.y > 0)
+            neighbours.push(Coordinate.create(this.x - 1, this.y - 1));
+        if(this.x < maxCoordinates.x && this.y < maxCoordinates.y)
+            neighbours.push(Coordinate.create(this.x + 1, this.y + 1));
+        if(this.x > 0 && this.y < maxCoordinates.y)
+            neighbours.push(Coordinate.create(this.x - 1, this.y + 1));
+        if(this.x < maxCoordinates.x && this.y > 0)
+            neighbours.push(Coordinate.create(this.x + 1, this.y - 1));
+
+        return neighbours;
+    },
 }
 
 function Bezier() {}
@@ -16,21 +41,22 @@ Bezier.create = function(c1, c2, c3, c4) {
     bezier.c2 = c2;
     bezier.c3 = c3;
     bezier.c4 = c4;
+    bezier.progress = 0;
 
     return bezier;
 }
 Bezier.prototype = {
-    point: function(progress) {
+    point: function() {
         var coordinate = Coordinate.create(0, 0);
 
-        coordinate.x = this.c1.x * this.b1(progress) + this.c2.x * this.b2(progress) + this.c3.x * this.b3(progress) + this.c4.x * this.b4(progress);
-        coordinate.y = this.c1.y * this.b1(progress) + this.c2.y * this.b2(progress) + this.c3.y * this.b3(progress) + this.c4.y * this.b4(progress);
+        coordinate.x = this.c1.x * this.b1(this.progress) + this.c2.x * this.b2(this.progress) + this.c3.x * this.b3(this.progress) + this.c4.x * this.b4(this.progress);
+        coordinate.y = this.c1.y * this.b1(this.progress) + this.c2.y * this.b2(this.progress) + this.c3.y * this.b3(this.progress) + this.c4.y * this.b4(this.progress);
 
         return coordinate;
     },
 
-    b1: function(t) { return t*t*t },
-    b2: function(t) { return 3*t*t*(1-t) },
-    b3: function(t) { return 3*t*(1-t)*(1-t) },
-    b4: function(t) { return (1-t)*(1-t)*(1-t) },
+    b1: function(t) { return (1-t)*(1-t)*(1-t) },
+    b2: function(t) { return 3*(1-t)*(1-t)*t },
+    b3: function(t) { return 3*(1-t)*t*t },
+    b4: function(t) { return t*t*t },
 }
--- js/ecosystem.js
+++ js/ecosystem.js
@@ -1,46 +1,35 @@
 function Ecosystem() {}
 Ecosystem.create = function(canvas) {
     var e = new Ecosystem()
-    e.birds = [];
-    e.addBird();
     e.canvas = canvas;
     e.ctx = canvas.getContext('2d');
-    return e
+
+    e.ants = [];
+    e.addAnt();
+    return e;
 }
 Ecosystem.prototype = {
     animate: function() {
         var that = this;
         setInterval(function() {
             that.tick()
-            that.draw()
         }, 20)
+    },
+
+    tick: function() {
+        this.draw()
+        for (var i in this.ants) {
+            var ant = this.ants[i]
+            ant.tick()
+        }
     },
 
     draw: function() {
         this.ctx.fillStyle = "#00a";
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
-
-        for (var i in this.birds) {
-            var bird = this.birds[i];
-            this.ctx.fillStyle = "#fff";
-
-            this.ctx.beginPath();
-            this.ctx.arc(bird.x, bird.y, 1, 0, Math.PI*2, true);
-            this.ctx.closePath();
-            this.ctx.fill();
-        }
     },
 
-    tick: function() {
-        for (var i in this.birds) {
-            var bird = this.birds[i]
-            bird.tick()
-                // if (bird.y < 0)
-            //     this.birds = _.without(this.birds, bird)
-        }
-    },
-
-    addBird: function() {
-        this.birds.push(Bird.create());
+    addAnt: function() {
+        this.ants.push(Ant.create(this));
     },
 }
